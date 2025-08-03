be03074f17d4dd109752f261ed6aed29ca075d9e
Nikhil Garbhana <76690992+NikhilGarbhana@users.noreply.github.com>
2025-08-03 14:49:50 +0530

    Update tvs.py

--- 2/tvs.py
+++ 2/tvs.py
@@ -26,7 +26,7 @@ import re
 
 # Setup Chrome WebDriver - Configure Selenium to use Chrome
 options = Options()
-# options.add_argument("--headless=new")  # Correct syntax for headless
+options.add_argument("--headless=new")  # Correct syntax for headless
 options.add_argument("--disable-popup-blocking")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
