42c0a5336271736f04f48c24d5ae12d89e92c22e
Nikhil Garbhana <76690992+NikhilGarbhana@users.noreply.github.com>
2025-08-07 11:12:08 +0530

    Update tvs.py

--- 3/tvs.py
+++ 3/tvs.py
@@ -131,7 +131,7 @@ def scrape_dealers(driver, city, state):
 def main():
     cities()
     driver, wait = start_browser()
-    for option_state in list(state_cities.keys())[24:30]:
+    for option_state in list(state_cities.keys())[30:33]:
         for k,option_city in enumerate(state_cities[option_state]):
         # for k,option_city in list(enumerate(state_cities[option_state]))[:2]:
             retry_count_city = 0
